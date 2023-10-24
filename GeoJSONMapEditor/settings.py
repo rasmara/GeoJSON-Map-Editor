3dbface91817a0e27c5c6affc967b99cc134d15c
Rasmara Rasheed <rasmara@Rasmaras-MacBook-Pro.local>
2023-10-24 20:19:23 +0400

    Reverted django secret key

--- GeoJSONMapEditor/settings.py
+++ GeoJSONMapEditor/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-n)o&s61h^%h&l%!ttmem5-5qzt3jry+p_(e*l56y)cao^s1d1g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
